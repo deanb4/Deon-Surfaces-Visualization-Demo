1d664cc0f68a87242e6fc2e9fe02ff1d2386ea5a
Dean <deanb258@gmail.com>
2025-05-10 19:44:41 -0500

    allowed hosts added for deployment

--- deonSurfacesDemo/settings.py
+++ deonSurfacesDemo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!)a(e#d%zgbuc(mktxjv2z%&g&+nuw3408=m4mk!k%3(6!&d#4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Deon-Surfaces-Visualization-Demo.onrender.com']
 
 
 # Application definition
